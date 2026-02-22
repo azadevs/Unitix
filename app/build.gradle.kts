2937c07823ed5948c28babb55e1821407e7b8679
Azamat <azamatdeveloper3@gmail.com>
2026-02-22 16:29:11 +0500

    Enabled DataStore dependency and updated its version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,7 +57,7 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
-//    implementation(libs.datastore)
+    implementation(libs.datastore)
     implementation(libs.lifecycle.viewmodel)
     implementation(libs.navigation.compose)
     implementation(libs.androidx.core.splashscreen)
